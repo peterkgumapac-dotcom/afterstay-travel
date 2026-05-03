25f9b83efd3b0b9b36d700f841d4be4be662f6c5
peter karl gumapac <peterkarlgumapac@peters-MacBook-Pro.local>
2026-05-03 03:17:55 -0500

    chore: add app verification hygiene

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: 'expo',
-  ignorePatterns: ['/dist/*', '/node_modules/*', '/ios/*', '/android/*'],
+  ignorePatterns: ['/dist/*', '/node_modules/*', '/ios/*', '/android/*', '/supabase/functions/*', '/scripts/*'],
   rules: {
     'react-hooks/exhaustive-deps': 'warn',
     '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
